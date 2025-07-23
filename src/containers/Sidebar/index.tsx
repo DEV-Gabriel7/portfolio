bae8f2da4df81849937134ed8d6c9c44df6b9f2b
Gabriel <gabrielriferreira@hotmail.com>
2025-07-23 19:17:07 -0300

    Criando a seção sobre e estilizando a página.

--- src/containers/Sidebar/index.tsx
+++ src/containers/Sidebar/index.tsx
@@ -1,9 +1,21 @@
+import Avatar from '../../components/Avatar'
+import Paragrafo from '../../components/Paragrafo'
 import Titulo from '../../components/Titulo'
+import { Descricao, BotaoTema, SidebarContainer } from './styles'
 
 const Sidebar = () => (
   <aside>
-    <img src="https://github.com/DEV-Gabriel7.png" />
-    <Titulo fontSize={20}>Gabriel Ribeiro Ferreira</Titulo>
+    <SidebarContainer>
+      <Avatar />
+      <Titulo fontSize={20}>Gabriel Ribeiro Ferreira</Titulo>
+      <Paragrafo tipo="secundario" fontSize={16}>
+        DEV-Gabriel7
+      </Paragrafo>
+      <Descricao tipo="principal" fontSize={12}>
+        Desenvolvedor Fullstack
+      </Descricao>
+      <BotaoTema>Trocar tema</BotaoTema>
+    </SidebarContainer>
   </aside>
 )
 
--- src/styles.ts
+++ src/styles.ts
@@ -3,19 +3,18 @@ import styled, { createGlobalStyle } from 'styled-components'
 const EstiloGlobal = createGlobalStyle`
   * {
     margin: 0;
-    paddiing: 0;
+    padding: 0;
     font-family: "Inter", sans-serif;
+    list-style: none;
   }
 
   body{
     padding-top: 80px;
+    padding-bottom: 80px;
 
     @media(max-width: 768px) {
       padding-top: 16px;
-
-
     }
-
   }
 `
 export default EstiloGlobal
@@ -31,13 +30,5 @@ export const Container = styled.div`
   @media (max-width: 768px) {
     max-width: 80%;
     display: block;
-
-    img {
-      width: 100%;
-    }
-  }
-
-  img {
-    max-width: 100%;
   }
 `
