4731ef0a07c75223a984ae456873c2fc6d9e92bf
Gabriel <gabrielriferreira@hotmail.com>
2025-07-22 23:10:41 -0300

    Criando a seção sobre mim

---
+++ src/containers/Sidebar/index.tsx
@@ -0,0 +1,10 @@
+import Titulo from '../../components/Titulo'
+
+const Sidebar = () => (
+  <aside>
+    <img src="https://github.com/DEV-Gabriel7.png" />
+    <Titulo fontSize={20}>Gabriel Ribeiro Ferreira</Titulo>
+  </aside>
+)
+
+export default Sidebar
